7a6b27d3767e691df9e62b396cc796e4061c0dd2
wwwdfwew <15237652609@163.com>
2024-11-24 14:58:51 +0800

    C代码

---
+++ C代码/代码.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	scanf_s("%d", &n);
+	printf("%d", n);
+	return 0;
+}
---
+++ C代码/源.cpp
@@ -0,0 +1,8 @@
+#include _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	return 0;
+}
