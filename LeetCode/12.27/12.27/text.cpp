f0da724cf1cec0233ea2d1810ffe5935384f78ff
wwwdfwew <15237652609@163.com>
2025-12-27 22:54:06 +0800

    爬梯子和x的平方根

---
+++ LeetCode/12.27/12.27/text.cpp
@@ -0,0 +1,25 @@
+
+
+//x的平方根
+//class Solution {
+//public:
+//    int mySqrt(int x) {
+//
+//        return (int)(sqrt(x));
+//    }
+//};
+
+//爬梯子
+//class Solution {
+//public:
+//    int climbStairs(int n) {
+//        int a = 1, b = 1, sum;
+//        for (int i = 0; i < n - 1; i++) {
+//            sum = a + b;
+//            a = b;
+//            b = sum;
+//        }
+//        return b;
+//    }
+//};
+
